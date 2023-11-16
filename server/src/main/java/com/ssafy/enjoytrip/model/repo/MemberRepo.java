0ff3a6c1c33dee98259fe0a0d7c114e3892872ed
iamyoungbin <binkorea2@gmail.com>
2023-11-16 10:05:44 +0900

    server jwt

--- server/src/main/java/com/ssafy/enjoytrip/model/repo/MemberRepo.java
+++ server/src/main/java/com/ssafy/enjoytrip/model/repo/MemberRepo.java
@@ -7,14 +7,17 @@ import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
 import java.sql.SQLException;
+import java.util.Map;
 
 @Mapper
 public interface MemberRepo {
 
-	int idCheck(String userId) throws SQLException;
-	int joinMember(MemberDto memberDto) throws SQLException;
+	MemberDto getUserInfo(String userId) throws Exception;
+	MemberDto loginMember(MemberDto memberDto) throws Exception;
 	int updateMember(MemberDto memberDto) throws SQLException;
 	int deleteMember(String userId) throws SQLException;
-	MemberDto loginMember(String userId) throws SQLException;
-	
+	int joinMember(MemberDto memberDto) throws SQLException;
+	void saveRefreshToken(Map<String, String> map) throws Exception;
+	Object getRefreshToken(String userId) throws Exception;
+	void deleteRefreshToken(Map<String, String> map) throws Exception;
 }
